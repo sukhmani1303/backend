62c958976c710237cb85cdb30d88fc7a065984e0
sukhmani1303 <sukhmani1303@gmail.com>
2023-02-21 16:10:18 +0530

    request body, driver code

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,8 @@
 # uvicorn main:app --reload
 from fastapi import FastAPI
 from typing import Optional
+import uvicorn
+
 app = FastAPI()
 
 @app.get('/')
@@ -34,6 +36,7 @@ def mul(id1 : int,id2 : int):
 def comm_fetch(id):
     return {'comments' : 'fetching the comments of id ' + id}
 
+
 ### Query Parameters ###
 
 # we can use query params with the help of '?'
@@ -52,3 +55,42 @@ def qry_prms(chk: bool = False ,limit = 0, tag = "", sort: Optional[str] = None)
         return {'data' : f'we have set limit to {limit} and tag to {tag}'}
     else:
         return {'data' : 'Cant access data !'}
+    
+
+### Request Body ###
+
+# we can test post requests from swagger Docs
+# we use request body to send data from client to API
+# we use Pydantic models
+from pydantic import BaseModel
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+# we have to create an API of our own to manage data
+# we create a Class with parameters and its Datatype
+# we declare the args in function with Datatype of Class
+
+@app.post('/blog1')
+def create_blog(request : Blog): # we can use any name for arg 
+    return request
+
+# we can create multiple such classes to return 
+# various types of data
+
+class B1(BaseModel):
+    a: int
+
+class B2(BaseModel):
+    b: int
+
+@app.post('/blog2')
+def cr_blog(i1: B1, i2: B2):
+    mul1 = i1.a*i2.b # using the '.' operator, we can access the Data Members
+    return {"data" : f"the multiplication is {mul1}"}
+
+# runner code
+if __name__ == "__main__":
+    uvicorn.run(app, host = "127.0.0.1", port = 9000)
